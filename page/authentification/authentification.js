36d4a3b6cb25d8c1a6f5949c185f9bd75d6569e8
cyrilcthd <cyril.cthd@gmail.com>
2023-02-14 20:18:26 +0100

    authentification+mailbox
    html + style authentification
    base de la mailbox
    ajout de dossier et sous dossier

---
+++ page/authentification/authentification.js
@@ -0,0 +1,21 @@
+
+function onload(){
+    let screenSizeHeight=window.innerHeight;
+    let screenSizeWidth=window.innerWidth;
+    let sectionSizeWidth=document.getElementById("formulaire").scrollWidth;
+    let sectionSizeHeight=document.getElementById("formulaire").scrollHeight;
+    document.getElementById("formulaire").style.marginTop =  screenSizeHeight/2-sectionSizeHeight/2+"px";
+    document.getElementById("formulaire").style.marginLeft =  screenSizeWidth/2-sectionSizeWidth/2+"px";
+    if (screenSizeWidth < screenSizeHeight){
+        document.getElementById("body").style.backgroundImage = "url('../img/backgroundAuthentificationRotate.webp')"
+        console.log("coucou")
+    }
+    else{
+        document.getElementById("body").style.backgroundImage = "url('../img/backgroundAuthentification.webp')"
+    }
+
+    let inputWidth = document.getElementById("email").scrollWidth
+    let sendButtonWidth = document.getElementById("sendButton").scrollWidth
+    document.getElementById("sendButton").style.marginLeft = inputWidth/2-sendButtonWidth/2+"px"
+}
+
