46edf3e13a82d6e1ad02906659256091e46160ec
cyrilcthd <cyril.cthd@gmail.com>
2023-02-15 03:57:26 +0100

    base

--- page/MailBox/mailBox.js
+++ page/MailBox/mailBox.js
@@ -3,6 +3,7 @@ function onload(){
     let windowWidthSize = window.innerWidth;
     let windowHeightSize = window.innerHeight;
     
+    document.getElementById("body").style.height = windowHeightSize-16+"px"
 
     document.getElementById("mailSender").style.marginBottom = windowHeightSize*0.01+"px";
     document.getElementById("mailSender").style.width = windowHeightSize*0.35+"px";
@@ -19,8 +20,22 @@ function onload(){
     document.getElementById("mailReceiveButton").style.width = windowWidthSize*0.2+"px";
     document.getElementById("mailSendButton").style.width = windowWidthSize*0.2+"px";
 
-    document.getElementById("searchBar").style.width = windowWidthSize*0.5+"px"
+    document.getElementById("searchBar").style.width = windowWidthSize*0.5+"px";
+
 }
+
+function changeBoxButtons(button){
+    if (button.id == "mailReceiveButton"){
+        document.getElementById("sendBox").style.display = "none";
+        document.getElementById("receptionBox").style.display = "inline";
+    }
+    else if(button.id == "mailSendButton"){
+        document.getElementById("receptionBox").style.display = "none";
+        document.getElementById("sendBox").style.display = "inline";
+    }
+}
+
+
 
 function newMailButton(button){
     if ( document.getElementById("mailSender").style.display == "none"){
