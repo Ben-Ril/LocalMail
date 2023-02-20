f8b51cf76da307a08532489387bae251ed15bd58
benjimania74 <lesdalton74@gmail.com>
2023-02-20 21:47:12 +0100

    Update authentification.js

--- page/authentification/authentification.js
+++ page/authentification/authentification.js
@@ -1,25 +1,3 @@
-
-/*function dd(){
-    let screenSizeHeight=window.innerHeight;
-    let screenSizeWidth=window.innerWidth;
-    console.log(screenSizeWidth,screenSizeHeight)
-    let sectionSizeWidth=document.getElementById("formulaire").scrollWidth;
-    let sectionSizeHeight=document.getElementById("formulaire").scrollHeight;
-    document.getElementById("formulaire").style.marginTop =  screenSizeHeight/2-sectionSizeHeight/2+"px";
-    document.getElementById("formulaire").style.marginLeft =  screenSizeWidth/2-sectionSizeWidth/2+"px";
-    if (screenSizeWidth < screenSizeHeight){
-        document.getElementById("body").style.backgroundImage = "url('../image/backgroundAuthentificationRotated.webp')"
-        
-    }
-    else{
-        document.getElementById("body").style.backgroundImage = "url('../image/backgroundAuthentification.webp')"
-    }
-
-    let inputWidth = document.getElementById("email").scrollWidth
-    let sendButtonWidth = document.getElementById("connectButton").scrollWidth
-    document.getElementById("sendButton").style.marginLeft = inputWidth/2-sendButtonWidth/2+"px"
-}*/
-
 function onload(){
     onresize();
 }
