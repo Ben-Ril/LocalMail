1f94d97993cb7647df5efc77d6614d39667d8a4d
cyrilcthd <115051089+cyrilcthd@users.noreply.github.com>
2023-02-21 11:28:20 +0100

    JS Update
    I've made it more readable

--- page/mailBox/mailBox.js
+++ page/mailBox/mailBox.js
@@ -1,53 +1,83 @@
-
 function onload(){
-    let windowWidthSize = window.innerWidth;
-    let windowHeightSize = window.innerHeight;
+    onresize();
+}
+
+
+function onresize(){
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const isHorizontal = screenHeight < screenWidth;
+
+    let body = document.getElementById("body");
+    let mailSender= document.getElementById("mailSender");
+    let mailObject = document.getElementById("mailObject");
+    let mailContent = document.getElementById("mailContent");
+    let mailReceiver = document.getElementById("mailReceiver");
+    let navMenu = document.getElementById("navMenu");
+    let newMailButton = document.getElementById("newMailButton");
+    let mailReceiveButton = document.getElementById("mailReceiveButton");
+    let mailSendedButton = document.getElementById("mailSendedButton");
+    let searchBar = document.getElementById("searchBar");
+
+    body.style.height = screenHeight-16+"px";
+
+    mailSender.style.marginBottom = screenHeight*0.01+"px";
+    mailSender.style.width = screenHeight*0.35+"px";
+    mailSender.style.bottom = "0px";
+    mailSender.style.left = screenWidth/2+"px";
+
+    mailObject.style.width = screenWidth*0.35+"px";
+
+    mailReceiver.style.width = screenWidth*0.35+"px";
+
+    mailContent.style.width = screenWidth*0.35+"px";
+    mailContent.style.height = screenHeight*0.35+"px";
     
-    document.getElementById("body").style.height = windowHeightSize-16+"px"
-
-    document.getElementById("mailSender").style.marginBottom = windowHeightSize*0.01+"px";
-    document.getElementById("mailSender").style.width = windowHeightSize*0.35+"px";
-    document.getElementById("object").style.width = windowWidthSize*0.35+"px";
-    document.getElementById("receiver").style.width = windowWidthSize*0.35+"px";
-    document.getElementById("content").style.width = windowWidthSize*0.35+"px";
-    document.getElementById("content").style.height = windowHeightSize*0.35+"px";
     
-    document.getElementById("mailSender").style.bottom = "0px";
-    document.getElementById("mailSender").style.left = windowWidthSize/2+"px";
 
-    document.getElementById("navMenu").style.width = windowWidthSize*0.2+"px";
-    document.getElementById("newMailButton").style.width = windowWidthSize*0.2+"px";
-    document.getElementById("mailReceiveButton").style.width = windowWidthSize*0.2+"px";
-    document.getElementById("mailSendButton").style.width = windowWidthSize*0.2+"px";
+    navMenu.style.width = screenWidth*0.2+"px";
 
-    document.getElementById("searchBar").style.width = windowWidthSize*0.5+"px";
+    newMailButton.style.width = screenWidth*0.2+"px";
+
+    mailReceiveButton.style.width = screenWidth*0.2+"px";
+
+    mailSendedButton.style.width = screenWidth*0.2+"px";
+
+    searchBar.style.width = screenWidth*0.5+"px";
 
 }
 
 function changeBoxButtons(button){
+
+    const sendedBox = document.getElementById("sendedBox")
+    const receptionBox = document.getElementById("receptionBox")
+    
     if (button.id == "mailReceiveButton"){
-        document.getElementById("sendBox").style.display = "none";
-        document.getElementById("receptionBox").style.display = "inline";
+        sendedBox.style.display = "none";
+        receptionBox.style.display = "inline";
     }
     else if(button.id == "mailSendButton"){
-        document.getElementById("receptionBox").style.display = "none";
-        document.getElementById("sendBox").style.display = "inline";
+        receptionBox.style.display = "none";
+        sendedBox.style.display = "inline";
     }
 }
 
 
 
 function newMailButton(button){
-    if ( document.getElementById("mailSender").style.display == "none"){
-        document.getElementById("mailSender").style.display = "inline";
+    const mailSender = document.getElementById("mailSender");
+
+    if ( mailSender.style.display == "none"){
+        mailSender.style.display = "inline";
     }
     else{
-        document.getElementById("mailSender").style.display = "none";
+        mailSender.style.display = "none";
     }
 
 }
 function getRickRoll(content){
     if (content.value == "rickroll") {
+        alert("YOU KNEW IT !");
         window.open('https://youtu.be/eR3fLy8d1PU', '_blank');
     }
 }
