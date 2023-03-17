9b98b8d72c35484cb19b4b1ce9ba69200d38f149
cyrilcthd <cyril.cthd@gmail.com>
2023-03-17 03:59:55 +0100

    update
    missing :
    -placement of newMail
    -better way to made buttons work
    -placement of navMenu

--- page/mailBox/mailBox.js
+++ page/mailBox/mailBox.js
@@ -1,95 +1,125 @@
-function onload(){
-    onresize();
+window.addEventListener("load", onWindowLoad);
+window.addEventListener("resize", onWindowResize);
+
+function onWindowLoad() {
+  onWindowResize();
 }
-function onresize(){
+function onWindowResize(){
     const screenHeight = window.innerHeight;
     const screenWidth = window.innerWidth;
     const isHorizontal = screenHeight < screenWidth;
-    const menuSearchContainerWidth = screenWidth*(isHorizontal? 0.6:0.98)
-    const menuSearchContainerHeight = screenHeight*(isHorizontal? 0.08:0.05)
-    const searchBarWidth = menuSearchContainerWidth*(isHorizontal? 0.8:0.8)
-    const searchBarHeight = menuSearchContainerHeight*0.6
-    const navMenuWidth = screenWidth*(isHorizontal? 0.3:0.7)
-    const navMenuHeight = screenHeight*(isHorizontal? 0.6:0.7)
-    const menuFontSize = searchBarHeight*0.8
-    const navMenuButtonWidth = navMenuWidth*0.6
-    const newMailButtonWidth = screenWidth*0.3
-    const mailBoxWidth = screenWidth*0.8
-    const mailBoxHeight = screenHeight*0.7
+    const menuSearchContainerWidth = screenWidth*(isHorizontal? 0.6:0.98);
+    const menuSearchContainerHeight = screenHeight*(isHorizontal? 0.08:0.05);
+    const searchBarWidth = menuSearchContainerWidth*(isHorizontal? 0.75:0.8);
+    const searchBarHeight = menuSearchContainerHeight*0.6;
+    const navMenuWidth = screenWidth*(isHorizontal? 0.3:0.7);
+    const navMenuHeight = screenHeight*(isHorizontal? 0.37:0.16);
+    const menuFontSize = searchBarHeight*0.8;
+    const navMenuButtonWidth = navMenuWidth*0.6;
+    const newMailButtonWidth = screenWidth*0.3;
+    const mailBoxWidth = screenWidth*0.97;
+    const mailBoxHeight = screenHeight*(isHorizontal?0.87:0.93);
+    const newMailWidth = screenWidth*(isHorizontal?0.4:0.7);
+    const newMailheight = screenHeight*(isHorizontal?0.6:0.7);
+    const newMailClassWidth = newMailWidth * 0.97;
+    const messageHeight = newMailheight*(isHorizontal?0.7:0.86);
+    const closeButtonWitdh = newMailClassWidth*0.15
 
 
-    let body = document.getElementById("body")
-    let menuSearchContainer = document.getElementById("menuSearchContainer")
-    let navMenu = document.getElementById("navMenu")
-    let MenuButton = document.getElementById("MenuButton")
-    let newMailButtonContainer = document.getElementById("newMailButtonContainer")
-    let newMailButton = document.getElementById("newMailButton")
-    let searchBar = document.getElementById("searchBar")
+    let body = document.getElementById("body");
+    let menuSearchContainer = document.getElementById("menuSearchContainer");
+    let navMenu = document.getElementById("navMenu");
+    let MenuButton = document.getElementById("MenuButton");
+    let newMailButtonContainer = document.getElementById("newMailButtonContainer");
+    let newMailButton = document.getElementById("newMailButton");
+    let searchBar = document.getElementById("searchBar");
+    let newMail = document.getElementById("newMail");
+    let newMailClass = document.getElementsByClassName("newMailClass");
+    let message = document.getElementById("message");
+    let navMenuButtons = document.getElementsByClassName("navMenuButtons");
+    let closeButton = document.getElementById("closeButton");
 
-    let navMenuButtonNew = document.getElementById("navMenuButtonNew")
-    let navMenuButtonReceived = document.getElementById("navMenuButtonReceived")
-    let navMenuButtonSended = document.getElementById("navMenuButtonSended")
-    let navMenuButtonAdmin = document.getElementById("navMenuButtonAdmin")
-
-    let receptionBox = document.getElementById("receptionBox")
-    let sendedBox = document.getElementById("sendedBox")
+    let receptionBox = document.getElementById("receptionBox");
+    let sendedBox = document.getElementById("sendedBox");
     
-    body.style.height = screenHeight-16+"px"
+    body.style.height = screenHeight-16+"px";
 
-    menuSearchContainer.style.width = menuSearchContainerWidth+"px"
-    menuSearchContainer.style.height = menuSearchContainerHeight+"px"
+    menuSearchContainer.style.width = menuSearchContainerWidth+"px";
+    menuSearchContainer.style.height = menuSearchContainerHeight+"px";
 
-    MenuButton.style.height = searchBarHeight + "px"
-    MenuButton.style.marginTop = (menuSearchContainerHeight-searchBarHeight)/2+"px"
-    MenuButton.style.fontSize = menuFontSize + "px"
+    MenuButton.style.height = searchBarHeight + "px";
+    MenuButton.style.marginTop = (menuSearchContainerHeight-searchBarHeight)/2+"px";
+    MenuButton.style.fontSize = menuFontSize + "px";
 
-    searchBar.style.width = searchBarWidth+"px"
-    searchBar.style.height = searchBarHeight+"px"
-    searchBar.style.marginTop = (menuSearchContainerHeight-searchBarHeight)/2-1+"px"
-    searchBar.style.fontSize = menuFontSize + "px"
+    searchBar.style.width = searchBarWidth+"px";
+    searchBar.style.height = searchBarHeight+"px";
+    searchBar.style.marginTop = (menuSearchContainerHeight-searchBarHeight)/2-1+"px";
+    searchBar.style.fontSize = menuFontSize + "px";
 
-    navMenu.style.width = navMenuWidth+"px"
-    navMenu.style.height = navMenuHeight+"px"
-    navMenu.style.fontSize = menuFontSize+"px"
+    navMenu.style.width = navMenuWidth+"px";
+    navMenu.style.height = navMenuHeight+"px";
+    navMenu.style.fontSize = menuFontSize+"px";
+    Array.prototype.forEach.call(navMenuButtons, function(elem){
+        elem.style.width = navMenuButtonWidth+"px";
+        elem.style.fontSize = menuFontSize+"px";
+        elem.style.marginLeft = navMenuWidth/2-navMenuButtonWidth/2+"px";
+    });
 
-    navMenuButtonNew.style.width = navMenuButtonWidth+"px"
-    navMenuButtonNew.style.fontSize = menuFontSize+"px"  //impossible de mettre une class aux bouttons et de tout appliquer d'un coup pour une raison osbcure
-    navMenuButtonNew.style.marginLeft = navMenuWidth/2-navMenuButtonWidth/2+"px"
-
-    navMenuButtonReceived.style.width = navMenuButtonWidth+"px"
-    navMenuButtonReceived.style.fontSize = menuFontSize+"px"
-    navMenuButtonReceived.style.marginLeft = navMenuWidth/2-navMenuButtonWidth/2+"px"
-
-    navMenuButtonSended.style.width = navMenuButtonWidth+"px"
-    navMenuButtonSended.style.fontSize = menuFontSize+"px"
-    navMenuButtonSended.style.marginLeft = navMenuWidth/2-navMenuButtonWidth/2+"px"
+    newMailButtonContainer.style.visibility = (isHorizontal? "hidden":"visible");
     
-    navMenuButtonAdmin.style.width = navMenuButtonWidth+"px"
-    navMenuButtonAdmin.style.fontSize = menuFontSize+"px"
-    navMenuButtonAdmin.style.marginLeft = navMenuWidth/2-navMenuButtonWidth/2+"px"
+    newMailButton.style.width = newMailButtonWidth+"px";
+    newMailButton.style.fontSize = menuFontSize+"px";
 
-    newMailButtonContainer.style.visibility = (isHorizontal? "hidden":"visible")
-    
-    newMailButton.style.width = newMailButtonWidth+"px"
-    newMailButton.style.fontSize = menuFontSize+"px"
+    newMail.style.width = newMailWidth+"px";
+    newMail.style.height = newMailheight+"px";
+    Array.prototype.forEach.call(newMailClass, function(elem) {
+        elem.style.fontSize = menuFontSize+"px";
+        elem.style.width = newMailClassWidth+"px" ;
+    });
+    message.style.height = messageHeight+"px";
+    closeButton.style.width = closeButtonWitdh+"px";
+    closeButton.style.fontSize = menuFontSize+"px";
 
-    //receptionBox.style.width = mailBoxWidth+"px"
-    //receptionBox.style.height = mailBoxHeight+"px"
 
-    //sendedBox.style.width = mailBoxWidth+"px"
-    //sendedBox.style.height = mailBoxHeight+"px"
+    receptionBox.style.width = mailBoxWidth+"px";
+    receptionBox.style.height = mailBoxHeight+"px";
+
+    sendedBox.style.width = mailBoxWidth+"px";
+    sendedBox.style.height = mailBoxHeight+"px";
 }
 
 function navMenuButton(button){
-    let navMenu = document.getElementById("navMenu")
-    let navMenuButton = document.getElementById("navMenuButton")
-    let navMenuButtonNew = document.getElementById("navMenuButtonNew")
+    let navMenu = document.getElementById("navMenu");
+    const isVisible = navMenu.style.visibility == "visible";
 
-    const isVisible = navMenu.style.visibility == "visible"
+    navMenu.style.visibility = (isVisible? "hidden":"visible");
+}
 
+function newMailButton(button){
+    let newMail = document.getElementById("newMail");
+    let newMailButtonContainer = document.getElementById("newMailButtonContainer");
+    const isVisible = newMail.style.visibility == "visible";
+    const screenHeight = window.innerHeight;
+    const screenWidth = window.innerWidth;
+    const isHorizontal = screenHeight < screenWidth;
 
-    navMenu.style.visibility = (isVisible? "hidden":"visible")
+    newMail.style.visibility = (isVisible? "hidden":"visible");
+    newMailButtonContainer.style.visibility = (isVisible? "visible":"hidden");
+    newMailButtonContainer.style.visibility = (isHorizontal? "hidden":"visible");
+}
+
+function mailBoxButton(button){
+    let receptionBox = document.getElementById("receptionBox");
+    let sendedBox = document.getElementById("sendedBox");
     
-    
+    switch(button.id){
+        case "navMenuButtonReceived": 
+            sendedBox.style.visibility = "hidden";
+            receptionBox.style.visibility = "visible";
+            break
+        case "navMenuButtonSended":
+            receptionBox.style.visibility = "hidden";
+            sendedBox.style.visibility = "visible";
+    }
 }
 
