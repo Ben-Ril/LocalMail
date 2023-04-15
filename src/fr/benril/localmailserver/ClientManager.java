a6cc7cf7f017c27d43919e2e8d81904c5a695590
benjimania74 <lesdalton74@gmail.com>
2023-04-15 23:19:05 +0200

    Correction of when no DB is set

--- src/fr/benril/localmailserver/ClientManager.java
+++ src/fr/benril/localmailserver/ClientManager.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 
 public class ClientManager implements Runnable {
     private final Socket client;
+    private boolean informedNoDB = false;
 
     public ClientManager(Socket client){
         this.client = client;
@@ -26,30 +27,48 @@ public class ClientManager implements Runnable {
             DataOutputStream writer = new DataOutputStream(client.getOutputStream());
             DataInputStream reader = new DataInputStream(client.getInputStream());
 
-            if(!DataBase.getInstance().isConnected()){
-                writer.writeUTF("noDB\n");
-                writer.flush();
-                writer.close();
-                client.close();
-                return;
+            boolean isDbGood = dbVerif(writer, reader);
+            if(isDbGood){
+                String message = reader.readUTF();
+                String[] request = message.split(" ");
+
+                if(request.length < 2){run();}
+
+                String action = request[0].toUpperCase();
+                String type = request[1].toUpperCase();
+                String info;
+                if(request.length == 2){
+                    info = "";
+                }else{
+                    info = message.replace(type + " " + action + " ", "");
+                }
+
+                new Request(RequestAction.valueOf(action), RequestType.valueOf(type), info, writer);
             }
-            String message = reader.readUTF();
-            String[] request = message.split(" ");
-
-            if(request.length < 2){run();}
-
-            String action = request[0].toUpperCase();
-            String type = request[1].toUpperCase();
-            String info;
-            if(request.length == 2){
-                info = "";
-            }else{
-                info = message.replace(type + " " + action + " ", "");
-            }
-
-            new Request(RequestAction.valueOf(action), RequestType.valueOf(type), info, writer);
         }catch (IOException ignore){}
         run();
+    }
+
+    private boolean dbVerif(DataOutputStream writer, DataInputStream reader) throws IOException {
+        if(!DataBase.getInstance().isConnected()){
+            if(!informedNoDB){
+                writer.writeUTF("noDB\n");
+                writer.flush();
+                informedNoDB = true;
+                return false;
+            }
+            String message = reader.readUTF();
+            if(message.length() != 0){
+                if(message.toUpperCase().startsWith("MODIFY DATABASE") && message.split(" ").length == 5){
+                    String[] explode = message.split(" ");
+                    String info = message.replace(explode[0] + " ", "").replace(explode[1] + " ", "");
+                    new Request(RequestAction.MODIFY, RequestType.DATABASE, info, writer);
+                    return DataBase.getInstance().isConnected();
+                }
+                return false;
+            }
+        }
+        return true;
     }
 
     public boolean isConnected(){
