9f64a948844ed09133115c5b0eb29c3869f7ca9a
benjimania74 <lesdalton74@gmail.com>
2023-04-26 16:26:45 +0200

    Add connections
    Admin panel connection
    Mailbox connection

---
+++ mailBox.php
@@ -0,0 +1,41 @@
+<?php
+include 'API.phar';
+$socketManager = new SocketManager();
+session_start();
+
+/* VERIF DB PAS DOWN
+// if(DOWN){
+    PAGE 'INDISPONNIBLE' 
+}*/
+
+$userManager = $socketManager->getUserManager();
+
+if(isset($_SESSION["connected"]) && isset($_SESSION["uuid"]) && $_SESSION["connected"] === true && $userManager->getUserByID($_SESSION["uuid"]) != null){
+    include('page/mailBox/mailBox.html');
+}else{
+    include("page/authentification/authentification.html");
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST["mail"]) && isset($_POST["password"])){
+        $mail = explode(".", $_POST["mail"]);
+        $name = $mail[0];
+        $firstname = explode("@", $mail[1])[0];
+        $password = $_POST["password"];
+
+        $user = $userManager->getUserByName($name, $firstname);
+        if($user === null){
+            // Utilisateur innexistant
+            header("location: mailbox.php");
+        }
+    
+        if($user->getPassword() === $password){
+            $_SESSION["connected"] = true;
+            $_SESSION["uuid"] = $user->getUUID();
+            header("location: mailbox.php");
+        }else{
+            header("location: mailbox.php");
+        }
+    }
+}
+?>
