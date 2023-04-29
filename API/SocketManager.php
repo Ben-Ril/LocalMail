2c3ff47b43405b2d50f866fae30063a081243b83
benjimania74 <lesdalton74@gmail.com>
2023-04-29 19:01:03 +0200

    Remove socket errors message when can't connect to server

--- API/SocketManager.php
+++ API/SocketManager.php
@@ -18,25 +18,29 @@ class SocketManager{
         $host = "127.0.0.1";
         $port = 15935;
 
-        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if ($this->socket === false) {
-            $this->error = true;
-            return;
-        }
+        try{
+            $this->socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            if (!$this->socket) {
+                $this->error = true;
+                return;
+            }
 
-        $result = socket_connect($this->socket, $host, $port);
-        if ($result === false) {
-            $this->error = true;
-            return;
-        }
+            if (!@socket_connect($this->socket, $host, $port)) {
+                $this->error = true;
+                return;
+            }
 
-        if($this->isDBConnected()){
-            $this->userManager = new UserManager($this);
-            $this->mailManager = new MailManager($this);
+            if($this->isDBConnected()){
+                $this->userManager = new UserManager($this);
+                $this->mailManager = new MailManager($this);
+            }
+        }catch(Exception $ex){
+            $this->error = true;
         }
     }
 
     public function isDBConnected(): bool {
+        if($this->error){return false;}
         $this->sendMessage("IS DB CONNECTED");
         $response = $this->readMessage(1);
         return $response == "YES";
