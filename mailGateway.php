fe95df6f8f15c85942428ef57217c57ad92a4fb0
benjimania74 <lesdalton74@gmail.com>
2023-05-03 12:59:58 +0200

    Finish the mailGateway

--- mailGateway.php
+++ mailGateway.php
@@ -5,17 +5,32 @@ foreach (glob("API/template/*.php") as $filename){include_once $filename;}
 require './LanguageManager.php';
 
 $socketManager = new SocketManager();
+if(!$socketManager->isDBConnected()){die("ERROR");}
 $languageManager = new LanguageManager();
 $mailManager = $socketManager->getMailManager();
+$userManager = $socketManager->getUserManager();
 
 session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
     if (isset($_GET["boxStatus"])){
+        $separator = "//<->//";
         $mails = $mailManager->getMailsByUser($_GET["uuid"], $_GET["boxStatus"] == "true");
-    }
-    foreach($mails as $mail){
-        echo($mail);
+
+        if($mails != null && count($mails) != 0){
+            foreach($mails as $mail){
+                $mailUUID = $mail->getMailUUID();
+                $mailSender = $mail->getSenderUUID();
+                $receivers = "";
+                foreach($mail->getReceiversUUIDs() as $receiverUUID){$receivers = $receivers . $receiverUUID . "<-->"; }
+                $receivers = substr_replace($receivers, "", -4, strlen($receivers));
+                $dateTimestamp = $mail->getDateTimestamp();
+                $object = $mail->getObject();
+                $content = $mail->getContent();
+
+                echo $mailUUID . $separator . $mailSender . $separator . $receivers . $separator . $dateTimestamp . $separator . $object . $separator . $content . "<br>";
+            }
+        }else{echo "NO MAIL";}
     }
 }
 ?>
